6138e04948ea216ed7ddfeeb3f891dac1bd52cc6
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-08-07 23:06:51 +0530

    d

--- src/routes/Auth/+page.svelte
+++ src/routes/Auth/+page.svelte
@@ -24,19 +24,6 @@
     measurementId: import.meta.env.VITE_MID,
   };
   if (getApps().length == 0) {
-<<<<<<< HEAD
-    // const app = initializeApp(firebaseConfig);
-    // const auth = getAuth(app);
-    // let email = "b@gmail.com";
-    // let pass = "123456";
-    // createUserWithEmailAndPassword(auth, email, pass)
-    //   .then((userCred) => {
-    //     console.log(userCred);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-=======
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
     let email = "c@gmail.com";
@@ -48,7 +35,6 @@
       .catch((err) => {
         console.log(err);
       });
->>>>>>> d0984eb (f)
   } else {
     undefined;
   }
